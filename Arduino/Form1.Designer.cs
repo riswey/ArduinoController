82fc883464ac3a9be553515dcb9987d3f4dc4bac
Alva Gosson <riswey@hotmail.com>
2018-08-25 16:07:12 +0100

    State ok. Com problems.

--- Arduino/Form1.Designer.cs
+++ Arduino/Form1.Designer.cs
@@ -29,11 +29,172 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudDesireSpeed = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCurrentSpeed = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnSetSpeed = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDesireSpeed)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.ErrorReceived += new System.IO.Ports.SerialErrorReceivedEventHandler(this.serialPort1_ErrorReceived);
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(292, 18);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nudDesireSpeed
+            // 
+            this.nudDesireSpeed.Location = new System.Drawing.Point(122, 20);
+            this.nudDesireSpeed.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.nudDesireSpeed.Name = "nudDesireSpeed";
+            this.nudDesireSpeed.Size = new System.Drawing.Size(71, 20);
+            this.nudDesireSpeed.TabIndex = 1;
+            this.nudDesireSpeed.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Desired Speed (Hz)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Current Speed (Hz)";
+            // 
+            // lblCurrentSpeed
+            // 
+            this.lblCurrentSpeed.AutoSize = true;
+            this.lblCurrentSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentSpeed.Location = new System.Drawing.Point(116, 58);
+            this.lblCurrentSpeed.Name = "lblCurrentSpeed";
+            this.lblCurrentSpeed.Size = new System.Drawing.Size(36, 37);
+            this.lblCurrentSpeed.TabIndex = 5;
+            this.lblCurrentSpeed.Text = "0";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(292, 64);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 103);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(379, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // btnSetSpeed
+            // 
+            this.btnSetSpeed.Location = new System.Drawing.Point(199, 18);
+            this.btnSetSpeed.Name = "btnSetSpeed";
+            this.btnSetSpeed.Size = new System.Drawing.Size(47, 23);
+            this.btnSetSpeed.TabIndex = 8;
+            this.btnSetSpeed.Text = "Set";
+            this.btnSetSpeed.UseVisualStyleBackColor = true;
+            this.btnSetSpeed.Click += new System.EventHandler(this.btnSetSpeed_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 125);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSetSpeed);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblCurrentSpeed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudDesireSpeed);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDesireSpeed)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nudDesireSpeed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCurrentSpeed;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button btnSetSpeed;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Arduino/Form1.cs
+++ Arduino/Form1.cs
@@ -1,19 +1,225 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using System.Threading.Tasks;
 
 namespace Arduino
 {
+
+    enum CMD: byte
+    {
+        DESIREDSPEED = 32,
+        START,
+        STOP,
+        REQUESTSPEED
+    };
+
+    enum STATE {Ready = 0, Running, Locked, Unlocked}       //2 bit
+    enum EVENT {Start = 0, Stop, Lock, Unlock}              //2 bit
+
     public partial class Form1 : Form
     {
+        string SIGNATURE = "\x0E\x0E";
+        string TERMINAL = "\n";
+        STATE state = STATE.Ready;
+        Task task = null;
+
         public Form1()
         {
             InitializeComponent();
+            serialPort1.Open();
+            serialPort1.DiscardInBuffer();  //clear anything
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (timer1 != null) timer1.Dispose();
+            if (task != null) task.Dispose();
+            if (serialPort1 != null) serialPort1.Close();
+        }
+
+        private void ProcessEvent(EVENT e) {
+
+            int matrix = ((byte)e << 2 | (byte)state);
+            //matrix 0...15            
+
+            //enum STATE { Ready = 0000, Running    = 0001, Locked  = 0010, Unlocked    = 0011}       //2 bit
+            //enum EVENT { Start = 0000, Stop       = 0100, Lock    = 1000, Unlock      = 1100}       //2 bit
+
+            switch (matrix)
+            {
+                case 0:     //Start + Ready
+                    ChangeState(EVENT.Start);
+                    break;
+                case 2:     //Start + Locked
+                    ChangeState(EVENT.Unlock);
+                    break;
+                case 3:     //Start + UnLocked
+                    ChangeState(EVENT.Lock);
+                    break;
+                case 5:     //Stop + Running
+                case 6:     //Stop + Locked
+                case 7:     //Stop + Unlocked
+                    ChangeState(EVENT.Stop);
+                    break;
+                case 9:     //Lock + Running
+                case 11:    //Lock + Unlocked
+                    ChangeState(EVENT.Lock);
+                    break;
+                case 14:    //Unlock + Locked
+                    ChangeState(EVENT.Unlock);
+                    break;
+            }
+        }
+
+        private void ChangeState(EVENT e)
+        {
+            switch (e)
+            {
+                case EVENT.Start:
+                    timer1.Start();
+                    state = STATE.Running;
+                    SendCommand(CMD.DESIREDSPEED);
+                    SendCommand(CMD.START);
+                    Task task = Task.Delay(5000).ContinueWith(t => ProcessEvent(EVENT.Lock));
+                    break;
+                case EVENT.Stop:
+                    if (this.task != null)
+                    {
+                        this.task.Dispose();
+                        this.task = null;
+                    }
+                    timer1.Stop();
+                    state = STATE.Ready;
+                    SendCommand(CMD.STOP);
+                    AsyncDisable(this.btnSetSpeed, false);
+                    AsyncText(btnStart, "Start");
+                    break;
+                case EVENT.Lock:
+                    state = STATE.Locked;
+                    AsyncText(btnStart, "Unlock");
+                    AsyncDisable(this.btnSetSpeed);
+                    break;
+                case EVENT.Unlock:
+                    state = STATE.Unlocked;
+                    AsyncText(btnStart, "Lock");
+                    AsyncDisable(this.btnSetSpeed, false);
+                    break;
+            }
+            //New state
+            AsyncText(toolStripStatusLabel1, state.ToString());
+        }
+
+        private void AsyncText(Control obj, string text, bool append = false)
+        {
+            if (append)
+            {
+                obj.Invoke(new Action<string>(n => obj.Text += n), new object[] { text });
+            }
+            else
+            {
+                obj.Invoke(new Action<string>(n => obj.Text = n), new object[] { text });
+            }
+        }
+
+        private void AsyncText(ToolStripLabel obj, string text)
+        {
+            obj.GetCurrentParent().Invoke(new Action<string>(n => obj.Text = n), new object[] { text });
+        }
+
+        private void AsyncDisable(Control obj, bool disable = true)
+        {
+            obj.Invoke(new Action(() => obj.Enabled = !disable));
+        }
+
+        void SendCommand(CMD cmd)
+        {
+            string data = "";
+            switch ((CMD)cmd)
+            {
+                case CMD.DESIREDSPEED:
+                    data = nudDesireSpeed.Value.ToString();
+                    break;
+            }
+
+            string packet = SerialEventsController.EncodePacket((byte)cmd, data);
+
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Write(packet);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    string packet = serialPort1.ReadLine();
+                    HandlePacket(packet);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        void HandlePacket(string packet)
+        {
+            string trimmed = packet.TrimEnd(Environment.NewLine.ToCharArray());
+            if (SerialEventsController.DecodePacket(trimmed, out byte cmd, out string data) != 0) return;
+
+            switch ((CMD)cmd)
+            {
+                case CMD.REQUESTSPEED:
+                    AsyncText(toolStripStatusLabel1, "Speed: " + data);
+
+                    AsyncText(lblCurrentSpeed, data );
+                    break;
+                default:
+                    AsyncText(toolStripStatusLabel1, "Unknown CMD: " + cmd.ToString() );
+                    break;
+            }
+        }
+
+        private void serialPort1_ErrorReceived(object sender, System.IO.Ports.SerialErrorReceivedEventArgs e)
+        {
+            MessageBox.Show(e.ToString());
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ProcessEvent(EVENT.Start);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            ProcessEvent(EVENT.Stop);
+        }
+
+        private void btnSetSpeed_Click(object sender, EventArgs e)
+        {
+            SendCommand(CMD.DESIREDSPEED);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendCommand(CMD.REQUESTSPEED);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendCommand(CMD.REQUESTSPEED);
         }
     }
 }
+
